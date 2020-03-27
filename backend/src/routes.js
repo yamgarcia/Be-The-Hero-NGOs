5f525aad7b6553c5640a6c0493d5569f4a73135a
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-03-26 23:42:48 -0700

    Incident Controller added

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,28 +2,29 @@ const express = require("express");
 const crypto = require("crypto");
 const connection = require("./database/connection");
 const ngocontroller = require("./controllers/NGOsControllers");
+const incidentController = require("./controllers/IncidentController");
 
 const routes = express.Router();
 
 /**
- * HTTP Methods:
- *
- * GET: Search/list information on the backend
- * POST: Create information on the backend
- * PUT: Change information on the backend
- * DELETE: Delete information on the backend
+ * @route    post /ngos
+ * @desc     Add NGOs to Database
+ * @access   Private
  */
+routes.post("/ngos", ngocontroller.create);
 
 /**
- * Parameter types:
- *   Query params: named and sent in the route after "?" (filters, paging)
- *     http://localhost:3333/users?name=Marcos || /users?page=2&name=Marcos
- *   Route params: used to identify resources
- *     '/users/:id' ->  http://localhost:3333/users/1
- *   Request body: used to create or change resources
+ * @route    get /ngos
+ * @desc     List all NGOs entries
+ * @access   Public
  */
-
-routes.post("/ngos", ngocontroller.create);
 routes.get("/ngos", ngocontroller.index);
+
+/**
+ * @route    post /incidents
+ * @desc     Add NGOs to Database
+ * @access   Public
+ */
+routes.post("/incidents", incidentController.create);
 
 module.exports = routes;
