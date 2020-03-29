0be17b09b1c5ad421a71be054b8f8f47e58554db
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-03-28 23:37:18 -0700

    React setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
-      <img src={logo} className='App-logo' alt='logo' />
-      <p>Omnistack week</p>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
