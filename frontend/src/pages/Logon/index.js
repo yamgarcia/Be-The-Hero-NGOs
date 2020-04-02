e10ce3f3e7fd18613dbac262e472ba70e7ca9ddb
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-04-02 10:58:34 -0700

    Home page styled

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -4,24 +4,29 @@ import "./styles.css";
 
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
+import { FiLogIn } from "react-icons/fi";
 
 const Logon = () => {
   return (
-    <div>
+    <>
       <div className='logon-container'>
         <section className='form'>
           <img src={logoImg} alt='logoImg' />
-          <form action=''>
+          <form>
             <h1>Logon</h1>
             <input placeholder='Your ID' />
-            <button type='submit'>Enter</button>
-
-            <a href='/register'>I don't have an account</a>
+            <button className='button' type='submit'>
+              Enter
+            </button>
+            <a href='/register'>
+              I don't have an account
+              <FiLogIn size={16} color='#E02041' />
+            </a>
           </form>
         </section>
+        <img src={heroesImg} alt='heroesImg' />
       </div>
-      <img src={heroesImg} alt='heroesImg' />
-    </div>
+    </>
   );
 };
 
