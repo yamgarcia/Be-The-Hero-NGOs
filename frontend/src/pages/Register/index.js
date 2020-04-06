7069f1497b319b20bf8d9c1e6c2b236860001365
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-04-05 23:57:11 -0700

    Register page connected to backend

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import logoImg from "../../assets/logo.svg";
 import { FiArrowLeft } from "react-icons/fi";
+//api
+import api from "../../services/api";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+
+  //History
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    //variables must have the same name as their controller in the req.body
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      state
+    };
+
+    try {
+      console.log(data);
+      const res = await api.post("/ngos", data);
+      alert(`Your ID access: ${res.data.id}`);
+
+      history.push("/");
+    } catch (error) {
+      alert("Register failed\n Try again");
+      console.error(error.message);
+    }
+  }
+
   return (
     <div className='register-container'>
       <div className='content'>
@@ -17,14 +52,37 @@ const Register = () => {
             <FiArrowLeft size={16} color='#E02041' />I already have an ID
           </Link>
         </section>
-        <form>
-          <input placeholder="NGO's name" />
-          <input type='email' placeholder='E-mail' />
-          <input placeholder='WhatsApp' />
+
+        <form onSubmit={handleRegister}>
+          <input
+            value={name}
+            onChange={e => setName(e.target.value)}
+            placeholder="NGO's name"
+          />
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            type='email'
+            placeholder='E-mail'
+          />
+          <input
+            value={whatsapp}
+            onChange={e => setWhatsapp(e.target.value)}
+            placeholder='WhatsApp'
+          />
 
           <div className='input-group'>
-            <input placeholder='City' />
-            <input placeholder='State' style={{ width: 180 }} />
+            <input
+              value={city}
+              onChange={e => setCity(e.target.value)}
+              placeholder='City'
+            />
+            <input
+              value={state}
+              onChange={e => setState(e.target.value)}
+              placeholder='State'
+              style={{ width: 180 }}
+            />
           </div>
 
           <button className='button' type='submit'>
