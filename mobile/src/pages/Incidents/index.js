e006ae0c6a57813b7f7885eaa1a0a3c97f695d0e
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-04-18 12:43:38 -0700

    FlatList fixed

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -23,18 +23,22 @@ const Incidents = () => {
   async function loadIncidents() {
     try {
       const response = await api.get("incidents");
-      const theData = response.data;
-      console.log("-----////////////////////////-----");
-      console.log(theData);
+      const theData = response.data.incidents;
       setIncidents(theData);
-      console.log(" ------------incidents array-------- ");
-      console.log(incidents);
-      console.log(" ------------incidents array end-------- ");
+      console.log(theData);
       setLoading(false);
     } catch (e) {
       console.error(e.message);
     }
   }
+
+  const listEmptyComponent = () => {
+    return (
+      <View>
+        <Text> loading... </Text>
+      </View>
+    );
+  };
 
   useEffect(() => {
     loadIncidents();
@@ -59,39 +63,162 @@ const Incidents = () => {
           <Text style={styles.description}>{loadingM}</Text>
         </View>
       ) : (
-        <View style={styles.incident}>
-          <Text style={styles.description}>LOADED</Text>
-        </View>
+        <FlatList
+          data={incidents}
+          style={styles.incidentList}
+          keyExtractor={(incident) => String(incident.id)}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={listEmptyComponent}
+          renderItem={({ item: incident }) => (
+            <View style={styles.incident}>
+              <Text style={styles.incidentProperty}>NGO:</Text>
+              <Text style={styles.incidentValue}>{incident.name}</Text>
 
-        // <FlatList
-        //   data={incidents}
-        //   style={styles.incidentList}
-        //   keyExtractor={(incident) => String(incident.id)}
-        //   showsVerticalScrollIndicator={false}
-        //   renderItem={({ item: incident }) => (
-        //     <View style={styles.incident}>
-        //       <Text style={styles.incidentProperty}>NGO:</Text>
-        //       <Text style={styles.incidentValue}>{incident.name}</Text>
+              <Text style={styles.incidentProperty}>Case:</Text>
+              <Text style={styles.incidentValue}>{incident.title}</Text>
 
-        //       <Text style={styles.incidentProperty}>Case:</Text>
-        //       <Text style={styles.incidentValue}>{incident.title}</Text>
+              <Text style={styles.incidentProperty}>Value:</Text>
+              <Text style={styles.incidentValue}>{incident.value}</Text>
 
-        //       <Text style={styles.incidentProperty}>Value:</Text>
-        //       <Text style={styles.incidentValue}>{incident.value}</Text>
-
-        //       <TouchableOpacity
-        //         style={styles.detailsButton}
-        //         onPress={navigateToDetail}
-        //       >
-        //         <Text style={styles.detailsButtonText}>See more Details</Text>
-        //         <Feather name={"arrow-right"} size={17} color={"#E02041"} />
-        //       </TouchableOpacity>
-        //     </View>
-        //   )}
-        // />
+              <TouchableOpacity
+                style={styles.detailsButton}
+                onPress={navigateToDetail}
+              >
+                <Text style={styles.detailsButtonText}>See more Details</Text>
+                <Feather name={"arrow-right"} size={17} color={"#E02041"} />
+              </TouchableOpacity>
+            </View>
+          )}
+        />
       )}
     </View>
   );
 };
 
 export default Incidents;
+// import React, { useState, useEffect } from "react";
+// import { Feather } from "@expo/vector-icons";
+// import { useNavigation } from "@react-navigation/native";
+// import { View, FlatList, Text, Image, TouchableOpacity } from "react-native";
+
+// import api from "../../services/api";
+
+// import logoImage from "../../assets/logo.png";
+// import styles from "./styles";
+
+// export default function Incidents() {
+//   const [incidents, setIncidents] = useState([]);
+//   const [totalIncidents, setTotalIncidents] = useState(0);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [loading, setLoading] = useState(false);
+
+//   const navigation = useNavigation();
+
+//   function navigateToDetails(incident) {
+//     navigation.navigate("Details", { incident });
+//   }
+
+//   async function loadIncidents() {
+//     if (loading) {
+//       return;
+//     }
+
+//     if (totalIncidents > 0 && incidents.length === totalIncidents) {
+//       return;
+//     }
+
+//     setLoading(true);
+//     try {
+//       const response = await api.get("/incidents", {
+//         params: {
+//           page: currentPage,
+//         },
+//       });
+
+//       // setIncidents(response.data.incidents);
+//       setIncidents([...incidents, ...response.data.incidents]);
+//       setTotalIncidents(response.headers["X-Total-Count"]);
+
+//       setCurrentPage(currentPage + 1);
+//       setLoading(false);
+
+//       // console.log(response.data);
+
+//       // setIncidents([...incidents, ...response.data]);
+//       // setTotalIncidents(response.headers["X-Total-Count"]);
+
+//       // setCurrentPage(currentPage + 1);
+//       // setLoading(false);
+//     } catch (e) {}
+//   }
+
+//   useEffect(() => {
+//     loadIncidents();
+//   }, []);
+
+//   const listEmptyComponent = () => {
+//     return (
+//       <View>
+//         <Text> loading </Text>
+//       </View>
+//     );
+//   };
+
+//   const renderList = () => {
+//     <FlatList
+//       data={incidents}
+//       style={styles.incidentsList}
+//       keyExtractor={(incident) => String(incident.id)}
+//       ListEmptyComponent={listEmptyComponent}
+//       showsVerticalScrollIndicator={false}
+//       onEndReached={loadIncidents}
+//       onEndReachedThreshold={0.2}
+//       renderItem={({ item: incident }) => (
+//         <View style={styles.incident}>
+//           <Text style={styles.incidentNGO}>
+//             {incident.name} from {incident.city}/{incident.state}
+//           </Text>
+//           <Text style={styles.incidentDescription}>{incident.description}</Text>
+//           <Text style={styles.incidentValue}>
+//             {Intl.NumberFormat("en-CA", {
+//               style: "currency",
+//               currency: "CAD",
+//             }).format(incident.value)}
+//           </Text>
+//           <TouchableOpacity
+//             onPress={() => navigateToDetails(incident)}
+//             style={styles.incidentButton}
+//           >
+//             <Text style={styles.incidentButtonText}>More details</Text>
+//             <Feather name='arrow-right' size={16} color='#E02041' />
+//           </TouchableOpacity>
+//         </View>
+//       )}
+//     />;
+//   };
+
+//   return (
+//     <View style={styles.incidentsContainer}>
+//       {console.log(incidents)}
+//       <View style={styles.headerContainer}>
+//         <Image source={logoImage} />
+//         <Text style={styles.headerText}>
+//           Total of <Text style={styles.headerTextBold}>{totalIncidents}</Text>{" "}
+//           incidents
+//         </Text>
+//       </View>
+//       <Text style={styles.mainTitle}>Bem-vindo</Text>
+//       <Text style={styles.mainDescription}>
+//         Chose one of the cases below to save the day!
+//       </Text>
+//       {/* {incidents.length === 0 ? (
+//         <Text style={styles.mainDescription}>
+//           Chose one of the cases below to save the day!
+//         </Text>
+//       ) : (
+//         renderList()
+//       )} */}
+//       {renderList()}
+//     </View>
+//   );
+// }
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -3,5 +3,4 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://192.168.0.20:3333",
 });
-//192.168.0.20:19001exp://192.168.0.20:19000exp://192.168.0.20:19000
 export default api;
